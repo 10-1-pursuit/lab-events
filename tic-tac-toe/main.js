2baf69df0bd3fcc6eea160334cfd7ee86f41c926
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-23 09:47:46 -0400

    solved lab

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -13,41 +13,43 @@ makeBoard();
 function makeMove() {
   let squares = document.querySelectorAll(".square");
   const voidant = document.querySelectorAll(".empty");
-  let count = 0
+  let count = 0;
 
   for (let box of squares) {
     for (let empty of voidant) {
-      box.addEventListener("click", () => {
+      box.addEventListener(
+        "click",
+        () => {
+          count++;
+          if (count % 2 === 0) {
+            box.textContent = "X";
+          } else {
+            box.textContent = "O";
+          }
 
-       count++;
-       if(count % 2=== 0){
-        box.textContent = "X"
-       }else{
-        box.textContent = "O"
-       }
-
-        if (box.classList.contains("empty")) {
-          box.classList.remove("empty");
-        }
-    
-      });
+          if (box.classList.contains("empty")) {
+            box.classList.remove("empty");
+          }
+        },
+        { once: true }
+      );
     }
   }
 }
 makeMove();
 
-
-
 const button = document.querySelector("button");
 
-function reset(){
-let squares = document.querySelectorAll(".square");
-for(let box of squares){
-    box.textContent=""
-}
+function reset() {
+  let squares = document.querySelectorAll(".square");
+
+  for (let box of squares) {
+    box.remove();
+  }
+  makeBoard();
 }
 
-button.addEventListener("click",()=>{
-    reset();
-})
-
+button.addEventListener("click", () => {
+  reset();
+  makeMove();
+});
