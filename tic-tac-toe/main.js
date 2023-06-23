a866d8fb3db51e2b8eedb3e7c930afb477303eac
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-22 20:31:35 -0400

    Solved all but one question

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,39 +1,53 @@
+const game = document.querySelector(".tic-tac-toe");
 
-const game = document.querySelector(".tic-tac-toe")
-
-function makeBoard(){
-    for(let i = 0; i<9;i++){
-        let div = document.createElement("div")
-        div.classList.add("empty")
-        div.classList.add("square")
-        game.append(div)
-    }
-//     for(let i = 0; i<9;i++){
-//         let div = document.createElement("div")
-//         div.classList.add("square")
-//         game.append(div)
-// }
+function makeBoard() {
+  for (let i = 0; i < 9; i++) {
+    let div = document.createElement("div");
+    div.classList.add("empty");
+    div.classList.add("square");
+    game.append(div);
+  }
 }
 makeBoard();
 
-function makeMove(){
-    let squares = document.querySelectorAll(".square");
-    const voidant = document.querySelectorAll(".empty")
+function makeMove() {
+  let squares = document.querySelectorAll(".square");
+  const voidant = document.querySelectorAll(".empty");
+  let count = 0
 
-    for(let box of squares){
-        
-        for(let empty of voidant){
-          
-        box.addEventListener("click", ()=>{
-            if(box.classList.contains("empty")){
-                box.textContent = "X";
-                empty.classList.remove()
-            }
-          
-        })
-       
-    }
+  for (let box of squares) {
+    for (let empty of voidant) {
+      box.addEventListener("click", () => {
+
+       count++;
+       if(count % 2=== 0){
+        box.textContent = "X"
+       }else{
+        box.textContent = "O"
+       }
+
+        if (box.classList.contains("empty")) {
+          box.classList.remove("empty");
+        }
     
+      });
+    }
+  }
+}
+makeMove();
+
+
+
+const button = document.querySelector("button");
+
+function reset(){
+let squares = document.querySelectorAll(".square");
+for(let box of squares){
+    box.textContent=""
 }
 }
-makeMove()
+
+button.addEventListener("click",()=>{
+    reset();
+})
+
